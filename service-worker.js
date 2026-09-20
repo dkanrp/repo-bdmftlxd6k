5080bd4b5b9bbc62d3a6eeddacacf6eac1390acf
dkanrp <les125583@gmail.com>
2026-09-20 23:05:16 +0900

    Initial note upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'ecn-cache-v1';
+var CORE_ASSETS = ['.', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(CORE_ASSETS).catch(function(){});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  if(event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      var network = fetch(event.request).then(function(res){
+        if(res && res.status === 200){
+          var copy = res.clone();
+          caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, copy); });
+        }
+        return res;
+      }).catch(function(){ return cached; });
+      return cached || network;
+    })
+  );
+});
